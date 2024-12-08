b9e598b1c497a7d2932466951436accb13426b13
KentaIkeda <ikeda@kanau-lab.co.jp>
2024-12-08 14:33:13 +0900

    cacooのワイヤーに沿ったレイアウト完成

---
+++ app/_components/MainVisual.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainVisual = () => {
+	return <div className='bg-gray-400 h-[700px]'>MainVisual</div>;
+};
+
+export default MainVisual;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import MainVisual from "@/app/_components/MainVisual";
+
 export default function Home() {
-	return <div>hello world</div>;
+	return (
+		<>
+			<MainVisual />
+			<p>RPG適性診断と10種類のRPGゲーム風のキャラクターで適性診断</p>
+			<div
+				id='image'
+				className='aspect-video w-full bg-gray-400 flex justify-center items-center'
+			>
+				ゲームキャラクターで結果が分かり親しみやすく自分の性格を知ることができる！
+				<br />
+				心理学に基づいたオリジナル適性診断。強味・性格・向いている環境などが分かる
+			</div>
+			<div>RPG適性診断を始める</div>
+			<div id='search-question'></div>
+			<button>診断結果を見る</button>
+		</>
+	);
 }
